c92521e14cf8df9ca5a13dd6a6e795a2ee80eff2
shusuke0812 <shu.ota0812@gmail.com>
2025-02-09 17:51:44 +0900

    QiitaSearchItemViewのタイトル部分を追加

--- qiita-reader-ios/QiitaReader/Screen/QiitaSearch/QiitaSearchItemView.swift
+++ qiita-reader-ios/QiitaReader/Screen/QiitaSearch/QiitaSearchItemView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct QiitaSearchItemView: View {
     var body: some View {
-        HeaderView()
+        VStack(spacing: 14) {
+            HeaderView()
+            TitleView()
+        }
     }
 }
 
@@ -26,12 +29,21 @@ private struct HeaderView: View {
             VStack(alignment: .leading, spacing: 8) {
                 Text("@qiita-tester")
                     .font(.system(size: 14))
+                    .lineLimit(1)
                 Text("2025年02月07日")
                     .font(.system(size: 14))
                     .foregroundStyle(.gray)
             }
             Spacer()
         }
+    }
+}
+
+private struct TitleView: View {
+    var body: some View {
+        Text("【Playwright】並列機能とシャーディングでテスト実行時間を大幅短縮！GitHub ActionsのCI設定実例付き")
+            .font(.system(size: 20, weight: .semibold))
+            .lineLimit(2)
     }
 }
 
