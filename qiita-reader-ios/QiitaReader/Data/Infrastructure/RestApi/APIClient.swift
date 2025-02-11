ef709b180193c2fa787735d6d1670f7cc47d30ef
shusuke0812 <shu.ota0812@gmail.com>
2025-02-11 20:32:58 +0900

    APIClient.start()にretry処理、レスポンスデータマッピング処理を追加

--- qiita-reader-ios/QiitaReader/Data/Infrastructure/RestApi/APIClient.swift
+++ qiita-reader-ios/QiitaReader/Data/Infrastructure/RestApi/APIClient.swift
@@ -8,6 +8,8 @@
 import Combine
 import Foundation
 
+// Ref: https://developer.apple.com/documentation/foundation/urlsession/processing_url_session_data_task_results_with_combine
+
 protocol APIClientProtocol {
     func start<T: APIRequestProtocol>(_ request: T) -> AnyPublisher<T.Response, Error>
 }
@@ -15,6 +17,7 @@ protocol APIClientProtocol {
 final class APIClient: APIClientProtocol {
     private let session: URLSession
     private let retryiesLeft: Int
+    private let jsonDecoder: JSONDecoder
 
     init(
         session: URLSession = URLSession.shared,
@@ -22,11 +25,16 @@ final class APIClient: APIClientProtocol {
     ) {
         self.session = session
         self.retryiesLeft = retryiesLeft
+        self.jsonDecoder = JSONDecoder()
+        jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
     }
 
     func start<T: APIRequestProtocol>(_ request: T) -> AnyPublisher<T.Response, Error> {
         return session
-            .dataTaskPublisher(for: request.buildRequest())
+            .dataTaskPublisher(for: request.buildUrlRequest())
+            .retry(retryiesLeft)
+            .map { $0.data }
+            .decode(type: T.Response.self, decoder: jsonDecoder)
             .eraseToAnyPublisher()
     }
 }
