83b9b691d560bc1070253eea0155f5e96281e6d5
shusuke0812 <shu.ota0812@gmail.com>
2025-02-11 19:22:34 +0900

    HTTPMethodを追加

---
+++ qiita-reader-ios/QiitaReader/Data/Infrastructure/HTTPMethod.swift
@@ -0,0 +1,16 @@
+//
+//  HTTPMethod.swift
+//  QiitaReader
+//
+//  Created by Shusuke Ota on 2025/2/11.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case path = "PATCH"
+    case delete = "DELETE"
+}
