6659a4d559b2b7ffdddfa72b35b43354ca6b00b2
shusuke0812 <shu.ota0812@gmail.com>
2025-02-16 14:43:32 +0900

    ItemListを追加

---
+++ qiita-reader-ios/QiitaReader/Data/Repository/Items/ItemList.swift
@@ -0,0 +1,16 @@
+//
+//  ItemList.swift
+//  QiitaReader
+//
+//  Created by Shusuke Ota on 2025/2/16.
+//
+
+import Foundation
+
+struct ItemList {
+    let list: [Item]
+
+    init(list: [Item]) {
+        self.list = list
+    }
+}
