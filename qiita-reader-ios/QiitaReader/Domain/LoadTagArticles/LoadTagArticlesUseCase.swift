fc5d1379cf16ffca2832d2b7a9a3f582854b8dad
shusuke0812 <shu.ota0812@gmail.com>
2025-02-23 00:19:36 +0900

    LoadTagArticlesUseCaseを修正
    - invoke()修正
    - retry, initialLoadを追加

---
+++ qiita-reader-ios/QiitaReader/Domain/LoadTagArticles/LoadTagArticlesUseCase.swift
@@ -0,0 +1,65 @@
+//
+//  LoadTagArticlesUseCase.swift
+//  QiitaReader
+//
+//  Created by Shusuke Ota on 2025/2/22.
+//
+
+import Combine
+import Foundation
+
+protocol LoadTagArticlesUseCaseProtocol {
+    func invoke(tagId: String, page: Int) -> AnyPublisher<TagArticle, APIError>
+}
+
+class LoadTagArticlesUseCase: LoadTagArticlesUseCaseProtocol {
+    private let tagRepository: TagRepositoryProtocol
+    private let tagItemsRepository: TagItemsRepositoryProtocol
+
+    private var itemList: TagItemList = TagItemList(list: [])
+    private var tag: Tag = Tag(id: "", iconUrlString: "", followersCount: 0, itemsCount: 0)
+
+    init(
+        tagRepository: TagRepositoryProtocol = TagRepository(),
+        tagItemsRepository: TagItemsRepositoryProtocol = TagItemsRepository()
+    ) {
+        self.tagRepository = tagRepository
+        self.tagItemsRepository = tagItemsRepository
+    }
+
+    func invoke(tagId: String, page: Int) -> AnyPublisher<TagArticle, APIError> {
+        if validateIsInitialLoad(tagId: tagId) {
+            return initialLoad(tagId: tagId, page: page)
+        }
+        return retry(page: page)
+    }
+
+    private func initialLoad(tagId: String, page: Int) -> AnyPublisher<TagArticle, APIError> {
+        Publishers.CombineLatest(
+            tagRepository.getTag(id: tagId),
+            tagItemsRepository.getItems(page: page, tagId: tagId)
+        )
+        .map { tag, itemList in
+            self.tag = tag
+            self.itemList = itemList
+            return TagArticle(tag: tag, itemList: itemList)
+        }
+        .eraseToAnyPublisher()
+    }
+
+    private func retry(page: Int) -> AnyPublisher<TagArticle, APIError> {
+        tagItemsRepository.getItems(page: page, tagId: self.tag.id)
+            .map { itemList in
+                self.itemList = self.itemList.add(items: itemList.list)
+                return TagArticle(tag: self.tag, itemList: self.itemList)
+            }
+            .eraseToAnyPublisher()
+    }
+
+    private func validateIsInitialLoad(tagId: String) -> Bool {
+        if self.tag.id == tagId {
+            return false
+        }
+        return true
+    }
+}
