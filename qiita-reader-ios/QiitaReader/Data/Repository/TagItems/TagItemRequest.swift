431b4feeb530bf8928ff64d1fba01d4fd291a622
shusuke0812 <shu.ota0812@gmail.com>
2025-02-22 22:59:23 +0900

    TagItemRequestを追加

---
+++ qiita-reader-ios/QiitaReader/Data/Repository/TagItems/TagItemRequest.swift
@@ -0,0 +1,44 @@
+//
+//  TagItemRequest.swift
+//  QiitaReader
+//
+//  Created by Shusuke Ota on 2025/2/22.
+//
+
+import Foundation
+
+struct TagItemRequest: APIRequestProtocol {
+    typealias Response = TagItem
+
+    private let tagId: String
+    private let page: Int
+    private let perPage: Int = 20
+
+    init(tagId: String, page: Int) {
+        self.tagId = tagId
+        self.page = page
+    }
+
+    var baseUrl: String {
+        EndPoint.qiita.urlString
+    }
+    var path: String {
+        "/tags/\(tagId)/items"
+    }
+    var method: HTTPMethod {
+        .get
+    }
+
+    var parameters: [URLQueryItem]? {
+        [
+            URLQueryItem(name: "page", value: String(page)),
+            URLQueryItem(name: "per_page", value: String(perPage)),
+        ]
+    }
+    var header: [String : String]? {
+        nil
+    }
+    var body: Data? {
+        nil
+    }
+}
