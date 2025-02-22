48c609711606ecfe9243e3778edf21cea8ed4aa9
shusuke0812 <shu.ota0812@gmail.com>
2025-02-22 22:51:16 +0900

    TagItemList型を追加

---
+++ qiita-reader-ios/QiitaReader/Data/Repository/TagItems/TagItemList.swift
@@ -0,0 +1,16 @@
+//
+//  TagItemList.swift
+//  QiitaReader
+//
+//  Created by Shusuke Ota on 2025/2/22.
+//
+
+import Foundation
+
+struct TagItemList {
+    let list: [TagItem]
+
+    init(list: [TagItem]) {
+        self.list = list
+    }
+}
