e0f476b303f0bd38f957760c39905bda46ba5bbc
shusuke0812 <shu.ota0812@gmail.com>
2025-02-22 23:59:12 +0900

    /Domain/LoadTagArticlesにUseCaseを移動、TagArticle型を追加

---
+++ qiita-reader-ios/QiitaReader/Domain/LoadTagArticles/TagArticle.swift
@@ -0,0 +1,13 @@
+//
+//  TagArticle.swift
+//  QiitaReader
+//
+//  Created by Shusuke Ota on 2025/2/22.
+//
+
+import Foundation
+
+struct TagArticle {
+    let tag: Tag
+    let itemList: ItemList
+}
