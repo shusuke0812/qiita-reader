358d972ed24aafa08a1b0cca512926a428296226
shusuke0812 <shu.ota0812@gmail.com>
2025-02-23 11:40:08 +0900

    TagArticlesViewのNavigationStackの中身を実装

--- qiita-reader-ios/QiitaReader/Screen/TagArticles/TagArticlesView.swift
+++ qiita-reader-ios/QiitaReader/Screen/TagArticles/TagArticlesView.swift
@@ -13,13 +13,25 @@ struct TagArticlesView<ViewModel: TagArticlesViewModelProtocol>: View {
 
     var body: some View {
         NavigationStack {
-            List {
-//                TagAttributeView(tag: )
-//                ArticleSearchItemView(item: ) { tag in
-//                }
-//                .listRowInsets(EdgeInsets())
+            VStack(alignment: .center, spacing: 8) {
+                TagAttributeView(tag: viewModel.output.tag)
+                List(viewModel.output.itemList.list) { item in
+                    ArticleSearchItemView(item: item) { tag in
+                    }
+                    .listRowInsets(EdgeInsets())
+                }
+                .listStyle(PlainListStyle())
             }
-            .listStyle(PlainListStyle())
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button(action: {
+                        dismiss()
+                    }, label: {
+                        Image(systemName: "xmark.circle")
+                            .foregroundStyle(.gray)
+                    })
+                }
+            }
         }
     }
 }
