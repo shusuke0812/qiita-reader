0b883c13b31c56e0a25340ae712b50166f099a3b
shusuke0812 <shu.ota0812@gmail.com>
2025-02-23 17:15:52 +0900

    ArticleDetailViewModelを追加

---
+++ qiita-reader-ios/QiitaReader/Screen/ArticleDetail/ArticleDetailViewModel.swift
@@ -0,0 +1,38 @@
+//
+//  ArticleDetailViewModel.swift
+//  QiitaReader
+//
+//  Created by Shusuke Ota on 2025/2/23.
+//
+
+import Foundation
+
+protocol ArticleDetailViewModelInput {
+}
+
+protocol ArticleDetailViewModelOutput {
+    var isLoading: Bool { get set }
+    var articleUrl: URL { get }
+}
+
+protocol ArticleDetailViewModelProtocol: ObservableObject {
+    var input: ArticleDetailViewModelInput { get }
+    var output: ArticleDetailViewModelOutput { get set }
+}
+
+class ArticleDetailViewModel: ArticleDetailViewModelProtocol, ArticleDetailViewModelInput, ArticleDetailViewModelOutput {
+    var input: ArticleDetailViewModelInput { self }
+    var output: ArticleDetailViewModelOutput { get { self } set {} }
+
+    // MARK: Output
+    @Published var isLoading: Bool = true
+    var articleUrl: URL {
+        URL(string: articleUrlString)!
+    }
+
+    private let articleUrlString: String
+
+    init(articleUrlString: String) {
+        self.articleUrlString = articleUrlString
+    }
+}
