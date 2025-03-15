b99a12072cf483c63112ecde0b4d206a486c87f1
shusuke0812 <shu.ota0812@gmail.com>
2025-03-15 18:11:38 +0900

    SFSymbol型を追加

---
+++ qiita_reader_ios/Shared/SFSymbol.swift
@@ -0,0 +1,25 @@
+//
+//  SFSymbol.swift
+//  QiitaReader
+//
+//  Created by Shusuke Ota on 2025/3/15.
+//
+
+import Foundation
+
+enum SFSymbol {
+    /// 􀊴 heart
+    static let heart = "heart"
+
+    /// 􀓤 person circle fill
+    static let personCircleFill = "person.circle.fill"
+
+    /// 􀉞 bookmark
+    static let bookmark = "bookmark"
+
+    /// 􀿪 question mark dashed
+    static let questionMarkDashed = "questionmark.app.dashed"
+
+    /// 􀁠 xmark circle
+    static let xmarkCircle = "xmark.circle"
+}
