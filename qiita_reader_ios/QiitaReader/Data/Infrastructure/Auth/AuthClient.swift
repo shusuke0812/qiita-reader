b161757f947dda0c808767208ee3d47db28de51a
shusuke0812 <shu.ota0812@gmail.com>
2025-03-25 09:43:44 +0900

    Add AuthClient and Env files

---
+++ qiita_reader_ios/QiitaReader/Data/Infrastructure/Auth/AuthClient.swift
@@ -0,0 +1,8 @@
+//
+//  AuthClient.swift
+//  QiitaReader
+//
+//  Created by Shusuke Ota on 2025/3/24.
+//
+
+import Foundation
---
+++ qiita_reader_ios/Shared/Config/Env.swift
@@ -0,0 +1,8 @@
+//
+//  Env.swift
+//  QiitaReader
+//
+//  Created by Shusuke Ota on 2025/3/25.
+//
+
+import Foundation
