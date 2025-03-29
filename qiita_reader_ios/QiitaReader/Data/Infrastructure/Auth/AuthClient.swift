35e4b4798301ba74b0520ca01b17180b5cd1038f
shusuke0812 <shu.ota0812@gmail.com>
2025-03-29 21:28:04 +0900

    AuthClientにASWebAuthenticationSessionを実行するメソッドを追加

--- qiita_reader_ios/QiitaReader/Data/Infrastructure/Auth/AuthClient.swift
+++ qiita_reader_ios/QiitaReader/Data/Infrastructure/Auth/AuthClient.swift
@@ -5,4 +5,31 @@
 //  Created by Shusuke Ota on 2025/3/24.
 //
 
+import AuthenticationServices
+import Combine
 import Foundation
+
+protocol AuthClientProtocol {
+    func start<T: AuthRequestProtocol>(_ request: T) -> AnyPublisher<URL?, Error>
+}
+
+class AuthClient: AuthClientProtocol {
+    func start<T: AuthRequestProtocol>(_ request: T) -> AnyPublisher<URL?, Error> {
+        return session(request)
+    }
+
+    private func session<T: AuthRequestProtocol>(_ request: T) -> AnyPublisher<URL?, Error> {
+        return Future<URL?, Error> { promise in
+            let session = ASWebAuthenticationSession(url: request.buildUrl()!, callbackURLScheme: request.callbackUrlScheme) { url, error in
+                if let error = error {
+                    promise(.failure(error))
+                    return
+                }
+                promise(.success(url))
+            }
+            session.prefersEphemeralWebBrowserSession = true
+            session.start()
+        }
+        .eraseToAnyPublisher()
+    }
+}
