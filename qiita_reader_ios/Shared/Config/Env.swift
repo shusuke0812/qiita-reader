6ef4fef2c6dfd706bcdf9d17df99e728a5d09c20
shusuke0812 <shu.ota0812@gmail.com>
2025-05-17 18:26:06 +0900

    Env.Qiita.clientIdとclientSecretが読み込めないバグを修正、scopeを追加

--- qiita_reader_ios/Shared/Config/Env.swift
+++ qiita_reader_ios/Shared/Config/Env.swift
@@ -10,7 +10,8 @@ import Foundation
 enum Env {
     /// Read theses data from xcconfig file
     enum Qiita {
-        static let clientId = Bundle.main.object(forInfoDictionaryKey: "QIITA_CLIENT_ID") as? String ?? ""
-        static let clientSecret = Bundle.main.object(forInfoDictionaryKey: "QIITA_CLIENT_SECRET") as? String ?? ""
+        static let clientId = Bundle.main.object(forInfoDictionaryKey: "ENV_QIITA_CLIENT_ID") as? String ?? ""
+        static let clientSecret = Bundle.main.object(forInfoDictionaryKey: "ENV_QIITA_CLIENT_SECRET") as? String ?? ""
+        static let scope = "read_qiita write_qiita"
     }
 }
