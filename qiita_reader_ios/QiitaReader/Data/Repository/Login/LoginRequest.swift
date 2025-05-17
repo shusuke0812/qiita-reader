6555e0a2431ffdf78d13271a3f8297ebf7af8ad1
shusuke0812 <shu.ota0812@gmail.com>
2025-05-17 18:25:34 +0900

    LoginRequestを追加

---
+++ qiita_reader_ios/QiitaReader/Data/Repository/Login/LoginRequest.swift
@@ -0,0 +1,36 @@
+//
+//  LoginRequest.swift
+//  QiitaReader
+//
+//  Created by Shusuke Ota on 2025/5/17.
+//
+
+import Foundation
+
+struct LoginRequest: AuthRequestProtocol {
+    typealias Response = String
+    
+    var baseUrl: String {
+        EndPoint.qiita.urlString
+    }
+    
+    var path: String {
+        "/oauth/authorize"
+    }
+    
+    var parameters: [URLQueryItem]? {
+        [
+            URLQueryItem(name: "client_id", value: Env.Qiita.clientId),
+            URLQueryItem(name: "scope", value: Env.Qiita.scope)
+        ]
+    }
+    
+    var callbackUrlScheme: String {
+        if let urlTypes = Bundle.main.object(forInfoDictionaryKey: "CFBundleURLTypes") as? [[String: Any]] {
+            if let urlScheme = urlTypes.first?["CFBundleURLSchemes"] as? [String] {
+                return urlScheme.first!
+            }
+        }
+        return "not-found"
+    }
+}
