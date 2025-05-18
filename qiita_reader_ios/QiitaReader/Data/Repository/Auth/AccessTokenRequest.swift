49aa905144ab8214cdb59a10f6442fc295fabd8b
shusuke0812 <shu.ota0812@gmail.com>
2025-05-18 13:04:11 +0900

    AccessTokenRequestのヘッダープロパティに値を追加

--- qiita_reader_ios/QiitaReader/Data/Repository/Auth/AccessTokenRequest.swift
+++ qiita_reader_ios/QiitaReader/Data/Repository/Auth/AccessTokenRequest.swift
@@ -29,7 +29,7 @@ struct AccessTokenRequest: APIRequestProtocol {
     }
 
     var header: [String : String]? {
-        nil
+        ["Content-Type": "application/json"]
     }
 
     var body: Data? {
