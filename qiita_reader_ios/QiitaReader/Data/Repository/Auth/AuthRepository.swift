2c8782995f5bb07250e7850a39052f5f90488832
shusuke0812 <shu.ota0812@gmail.com>
2025-05-18 13:22:34 +0900

    AccessTokenを取得するAPIの呼び出し処理をAuthRepositoryに追加

--- qiita_reader_ios/QiitaReader/Data/Repository/Auth/AuthRepository.swift
+++ qiita_reader_ios/QiitaReader/Data/Repository/Auth/AuthRepository.swift
@@ -10,14 +10,20 @@ import Foundation
 
 protocol AuthRepositoryProtocol {
     func authorize() -> AnyPublisher<Authorize, AuthError>
+    func getAccessToken(authorizedCode: String) -> AnyPublisher<AuthToken, APIError>
 }
 
 class AuthRepository: AuthRepositoryProtocol {
     private let authClient: AuthClientProtocol
+    private let apiClient: APIClientProtocol
     private var cancellables: Set<AnyCancellable> = []
 
-    init(authClient: AuthClientProtocol = AuthClient()) {
+    init(
+        authClient: AuthClientProtocol = AuthClient(),
+        apiClient: APIClientProtocol = APIClient()
+    ) {
         self.authClient = authClient
+        self.apiClient = apiClient
     }
 
     func authorize() -> AnyPublisher<Authorize, AuthError> {
@@ -27,6 +33,13 @@ class AuthRepository: AuthRepositoryProtocol {
             .map { url in
                 return Authorize(url: url)
             }
+            .eraseToAnyPublisher()
+    }
+
+    func getAccessToken(authorizedCode: String) -> AnyPublisher<AuthToken, APIError> {
+        let request = AccessTokenRequest(authorizedCode: authorizedCode)
+        return apiClient
+            .start(request)
             .eraseToAnyPublisher()
     }
 }
