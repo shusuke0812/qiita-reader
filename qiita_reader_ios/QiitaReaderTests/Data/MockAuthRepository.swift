31da7dd66e0242679e746bf696a50f8ea48902ee
shusuke0812 <shu.ota0812@gmail.com>
2025-05-21 23:40:22 +0900

    MockAuthRepositoryで設定したtokenおよびcodeの桁数を修正（64->40）

--- qiita_reader_ios/QiitaReaderTests/Data/MockAuthRepository.swift
+++ qiita_reader_ios/QiitaReaderTests/Data/MockAuthRepository.swift
@@ -13,14 +13,14 @@ class MockAuthRepository: AuthRepositoryProtocol {
     var accessToken: String?
 
     func authorize() -> AnyPublisher<Authorize, AuthError> {
-        let authorize = Authorize(url: URL(string: "appsheme://xxx-callback?code=fefef5f067171f247fb415e38cb0631797b82f4141dcdee66db846c3ade57a03"))
+        let authorize = Authorize(url: URL(string: "appsheme://xxx-callback?code=fefef5f067171f247fb415e38cb0631797b82f41"))
         return Just(authorize)
             .setFailureType(to: AuthError.self)
             .eraseToAnyPublisher()
     }
     
     func generateAccessToken(authorizedCode: String) -> AnyPublisher<AuthToken, APIError> {
-        let authToken = AuthToken(accessToken: "ea5d0a593b2655e9568f144fb1826342292f5c6b7d406fda00577b8d1530d8a5")
+        let authToken = AuthToken(accessToken: "ea5d0a593b2655e9568f144fb1826342292f5c6b")
         return Just(authToken)
             .setFailureType(to: APIError.self)
             .eraseToAnyPublisher()
