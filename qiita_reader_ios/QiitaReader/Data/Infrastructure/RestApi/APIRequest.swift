a52d96ecef88ca21b5485440db86fa9d1ae60b19
shusuke0812 <shu.ota0812@gmail.com>
2025-05-22 22:32:56 +0900

    APIRequestのbuildUrlRequest()をpostに対応させた

--- qiita_reader_ios/QiitaReader/Data/Infrastructure/RestApi/APIRequest.swift
+++ qiita_reader_ios/QiitaReader/Data/Infrastructure/RestApi/APIRequest.swift
@@ -10,22 +10,33 @@ import Foundation
 protocol APIRequestProtocol {
     associatedtype Response: Decodable
 
+    typealias HTTPBody = [String: Any]
+
     var baseUrl: String { get }
     var path: String { get }
     var method: HTTPMethod { get }
     var parameters: [URLQueryItem]? { get }
     var header: [String: String]? { get }
-    var body: Data? { get }
+    var body: HTTPBody? { get }
 }
 
 extension APIRequestProtocol {
     func buildUrlRequest() -> URLRequest {
         let url = URL(string: baseUrl.appending(path))!
         var components = URLComponents(url: url, resolvingAgainstBaseURL: true)
+        var httpBody: Data?
 
         switch method {
         case .get:
             components?.queryItems = parameters
+        case .post:
+            if let body = body {
+                do {
+                    httpBody = try JSONSerialization.data(withJSONObject: body, options: [])
+                } catch {
+                    fatalError("Failed to serialize JSON body: \(error)")
+                }
+            }
         default:
             fatalError("Unimplemented HTTP mehotd: \(method)")
         }
@@ -33,6 +44,7 @@ extension APIRequestProtocol {
         var urlRequest = URLRequest(url: url)
         urlRequest.url = components?.url
         urlRequest.httpMethod = method.rawValue
+        urlRequest.httpBody = httpBody
         if let header = header {
             for (key, value) in header {
                 urlRequest.addValue(value, forHTTPHeaderField: key)
