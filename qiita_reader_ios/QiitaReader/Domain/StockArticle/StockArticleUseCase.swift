5a1341570a1749a8fcbe0f0763e34219830d051a
shusuke0812 <shu.ota0812@gmail.com>
2025-05-22 23:03:35 +0900

    StockArticleUseCaseにてサインインされていない場合はサインインを実行するようにした

--- qiita_reader_ios/QiitaReader/Domain/StockArticle/StockArticleUseCase.swift
+++ qiita_reader_ios/QiitaReader/Domain/StockArticle/StockArticleUseCase.swift
@@ -9,13 +9,28 @@ import Combine
 import Foundation
 
 protocol StockArticleUseCaseProtocol {
-    func invoke(itemId: Int) -> AnyPublisher<Void, Error>
+    func invoke(itemId: Int) -> AnyPublisher<Void, SignInError>
 }
 
 class StockArticleUseCase: StockArticleUseCaseProtocol {
-    func invoke(itemId: Int) -> AnyPublisher<Void, Error> {
-        return Just<Void>(())
-            .setFailureType(to: Error.self)
-            .eraseToAnyPublisher()
+    private let validateUserCredentialUseCase: ValidateUserCredentialUseCaseProtocol
+    private let signInUseCase: SignInUseCaseProtocol
+
+    init(
+        validateUserCredentialUseCase: ValidateUserCredentialUseCaseProtocol = ValidateUserCredentialUseCase(),
+        signInUseCase: SignInUseCaseProtocol = SignInUseCase()
+    ) {
+        self.validateUserCredentialUseCase = validateUserCredentialUseCase
+        self.signInUseCase = signInUseCase
+    }
+    // TODO: 未ログインの場合はログイン画面を表示. ログイン済みかつストック済みである場合はストック削除メソッドを呼ぶ
+    // TODO: エラー型をStockErrorに修正する
+    func invoke(itemId: Int) -> AnyPublisher<Void, SignInError> {
+        if validateUserCredentialUseCase.isValid {
+            return Just<Void>(())
+                .setFailureType(to: SignInError.self)
+                .eraseToAnyPublisher()
+        }
+        return signInUseCase.signIn()
     }
 }
