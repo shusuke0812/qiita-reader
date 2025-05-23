fc5a8162caeb98b0716e0de4138597bd6bb8dbc5
shusuke0812 <shu.ota0812@gmail.com>
2025-05-23 09:36:28 +0900

    SignOutUseCaseを追加

---
+++ qiita_reader_ios/QiitaReader/Domain/SignOut/SignOutUseCase.swift
@@ -0,0 +1,28 @@
+//
+//  SignOutUseCase.swift
+//  QiitaReader
+//
+//  Created by Shusuke Ota on 2025/5/23.
+//
+
+import Combine
+import Foundation
+
+protocol SignOutUseCaseProtocol {
+    func signOut() -> AnyPublisher<Void, Error>
+}
+
+class SignOutUseCase: SignOutUseCaseProtocol {
+    private let authRepository: AuthRepositoryProtocol
+
+    init(authRepository: AuthRepositoryProtocol = AuthRepository()) {
+        self.authRepository = authRepository
+    }
+
+    func signOut() -> AnyPublisher<Void, Error> {
+        authRepository.deleteAccessToken()
+        return Just<Void>(())
+            .setFailureType(to: Error.self)
+            .eraseToAnyPublisher()
+    }
+}
