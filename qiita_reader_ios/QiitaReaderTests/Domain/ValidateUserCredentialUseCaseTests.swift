2d7863757c3e4636e6f3c6844036dfed32050cd3
shusuke0812 <shu.ota0812@gmail.com>
2025-05-24 09:58:28 +0900

    test_isValidのメソッド名を修正

--- qiita_reader_ios/QiitaReaderTests/Domain/ValidateUserCredentialUseCaseTests.swift
+++ qiita_reader_ios/QiitaReaderTests/Domain/ValidateUserCredentialUseCaseTests.swift
@@ -24,7 +24,7 @@ struct ValidateUserCredentialUseCaseTests {
     }
 
     /// 無効なアクセストークン（長さ超過）
-    @Test func test_isValid_true_whenLongLengthAccessToken() {
+    @Test func test_isValid_false_whenLongLengthAccessToken() {
         mockAuthRepository.accessToken = "ea5d0a593b2655e9568f144fb1826342292f5c6b6b6b"
 
         let useCase = ValidateUserCredentialUseCase(authRepository: mockAuthRepository)
@@ -32,7 +32,7 @@ struct ValidateUserCredentialUseCaseTests {
     }
 
     /// 無効なアクセストークン（長さ不足）
-    @Test func test_isValid_true_whenShortLengthAccessToken() {
+    @Test func test_isValid_false_whenShortLengthAccessToken() {
         mockAuthRepository.accessToken = "ea5d0a593b2655e9568f144fb1826342292f"
 
         let useCase = ValidateUserCredentialUseCase(authRepository: mockAuthRepository)
@@ -40,7 +40,7 @@ struct ValidateUserCredentialUseCaseTests {
     }
 
     /// 無効なアクセストークン（nil）
-    @Test func test_isValid_true_whenAccessTokenIsNil() {
+    @Test func test_isValid_false_whenAccessTokenIsNil() {
         mockAuthRepository.accessToken = nil
 
         let useCase = ValidateUserCredentialUseCase(authRepository: mockAuthRepository)
@@ -48,7 +48,7 @@ struct ValidateUserCredentialUseCaseTests {
     }
 
     /// 無効なアクセストークン（空文字）
-    @Test func test_isValid_true_whenAccessTokenIsEmpty() {
+    @Test func test_isValid_false_whenAccessTokenIsEmpty() {
         mockAuthRepository.accessToken = ""
 
         let useCase = ValidateUserCredentialUseCase(authRepository: mockAuthRepository)
@@ -56,7 +56,7 @@ struct ValidateUserCredentialUseCaseTests {
     }
 
     /// 無効なアクセストークン（大文字のアルファベットを含む）
-    @Test func test_isValid_true_whenAccessTokenContainsUpperCases() {
+    @Test func test_isValid_false_whenAccessTokenContainsUpperCases() {
         mockAuthRepository.accessToken = "EA5D0A593B2655E9568F144FB1826342292F5C6B"
 
         let useCase = ValidateUserCredentialUseCase(authRepository: mockAuthRepository)
