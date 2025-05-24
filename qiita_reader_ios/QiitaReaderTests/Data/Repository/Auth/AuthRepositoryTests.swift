bf1fe837bfa98ac98b25bda16d830a47a13dec05
shusuke0812 <shu.ota0812@gmail.com>
2025-05-24 12:09:43 +0900

    AuthRepositoryTestsを修正

--- qiita_reader_ios/QiitaReaderTests/Data/Repository/Auth/AuthRepositoryTests.swift
+++ qiita_reader_ios/QiitaReaderTests/Data/Repository/Auth/AuthRepositoryTests.swift
@@ -5,12 +5,68 @@
 //  Created by Shusuke Ota on 2025/5/23.
 //
 
+import Combine
+import Foundation
 import Testing
+@testable import QiitaReader
 
-struct AuthRepositoryTests {
+class AuthRepositoryTests {
+    private let authRepository: AuthRepositoryProtocol
+    private var secureStorageClient: MockSecureStorageClient
 
-    @Test func <#test function name#>() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    private var cancellables: Set<AnyCancellable> = []
+
+    init() {
+        self.secureStorageClient = MockSecureStorageClient()
+        self.authRepository = AuthRepository(secureStorageClient: secureStorageClient)
     }
 
+    @Test func test_setAccessToken_true() async {
+        let expectedAccessToken = "ea5d0a593b2655e9568f144fb1826342292f5c6b"
+
+        await confirmation("success") { expectation in
+            authRepository
+                .setAccessToken(expectedAccessToken)
+                .sink(receiveCompletion: { _ in
+                    expectation()
+                }, receiveValue: { _ in
+                })
+                .store(in: &cancellables)
+        }
+
+        #expect(secureStorageClient.savedStringValue == expectedAccessToken)
+    }
+
+    @Test func test_setAccessToken_false_whenDuplicateToken() async {
+        let expectedAccessToken = "ea5d0a593b2655e9568f144fb1826342292f5c6b"
+        secureStorageClient.error = SecureStorageError.failedToSet(status: errSecDuplicateItem)
+
+        await confirmation("fail") { expectation in
+            authRepository
+                .setAccessToken(expectedAccessToken)
+                .sink(receiveCompletion: { _ in
+                    expectation()
+                }, receiveValue: { _ in
+                })
+                .store(in: &cancellables)
+        }
+
+        #expect(secureStorageClient.savedStringValue == nil)
+    }
+
+    @Test func test_setAccessToken_false_whenEmptyToken() async {
+        let expectedAccessToken = ""
+
+        await confirmation("fail") { expectation in
+            authRepository
+                .setAccessToken(expectedAccessToken)
+                .sink(receiveCompletion: { _ in
+                    expectation()
+                }, receiveValue: { _ in
+                })
+                .store(in: &cancellables)
+        }
+
+        #expect(secureStorageClient.savedStringValue == nil)
+    }
 }
