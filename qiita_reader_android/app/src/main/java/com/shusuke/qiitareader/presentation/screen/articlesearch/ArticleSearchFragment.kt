07ee9d31f55c1d4ef30ed67a2414aa0863312b39
shusuke0812 <shu.ota0812@gmail.com>
2025-08-02 10:28:07 +0900

    ArticleSearchFragmentを追加

---
+++ qiita_reader_android/app/src/main/java/com/shusuke/qiitareader/presentation/screen/articlesearch/ArticleSearchFragment.kt
@@ -0,0 +1,22 @@
+package com.shusuke.qiitareader.presentation.screen.articlesearch
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
+
+class ArticleSearchFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                ArticleSearchScreen()
+            }
+        }
+    }
+}
