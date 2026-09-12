4eb829bda0e1ea827b6857ecd61b4eb687677c18
shusuke0812 <shu.ota0812@gmail.com>
2026-09-12 13:07:35 +0900

    ArticleSearchUiState改良（画面の状態を定義するようにした）

--- qiita_reader_android/app/src/main/java/com/shusuke/qiitareader/presentation/screen/articlesearch/ArticleSearchUiState.kt
+++ qiita_reader_android/app/src/main/java/com/shusuke/qiitareader/presentation/screen/articlesearch/ArticleSearchUiState.kt
@@ -1,17 +1,25 @@
 package com.shusuke.qiitareader.presentation.screen.articlesearch
 
 import com.shusuke.qiitareader.data.repository.items.ItemList
-import com.shusuke.qiitareader.presentation.screen.setting.AppLanguage
 
-data class ArticleSearchUiState(
-    val query: String = "",
-    val isLoading: Boolean = false,
-    val content: ArticleSearchContent = ArticleSearchContent.Standby,
-    val language: AppLanguage = AppLanguage.JAPANESE
-) {
-    sealed class ArticleSearchContent {
-        data object Standby : ArticleSearchContent()
-        data class Success(val itemList: ItemList) : ArticleSearchContent()
-        data class Failure(val error: ArticleSearchError) : ArticleSearchContent()
+sealed class ArticleSearchUiState {
+    data object Initial : ArticleSearchUiState()
+    data object Loading : ArticleSearchUiState()
+    sealed class Searched : ArticleSearchUiState() {
+        abstract val itemList: ItemList
+
+        data class List(
+            override val itemList: ItemList
+        ) : Searched()
+
+        data class PageLoading(
+            override val itemList: ItemList
+        ) : Searched()
+
+        data class PageError(
+            override val itemList: ItemList,
+            val error: ArticleSearchError
+        ) : Searched()
     }
+    data class SearchError(val error: ArticleSearchError) : ArticleSearchUiState()
 }
